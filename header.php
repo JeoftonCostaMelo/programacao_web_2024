5c4c1f99ebe17f548320e687d31651bf49c49a48
JeoftonCosta <jeofton_costa@hotmail.com>
2024-11-23 16:05:48 -0300

    Template do projeto da disciplina de Programação Web

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Locadora de Filmes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Locadora de Filmes</h1>
+        <nav>
+            <a href="index.php">Início</a> | 
+            <a href="create.php">Adicionar Filme</a>
+        </nav>
+        <hr>
+    </div>
